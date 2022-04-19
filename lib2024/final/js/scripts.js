f8469e18367eac9cae943a63b4ed91f6aea14c8d
alpalgon1 <97483193+alpalgon1@users.noreply.github.com>
2022-04-18 23:23:08 -0400

    Final
    Final

---
+++ lib2024/final/js/scripts.js
@@ -0,0 +1,9 @@
+function showHideMobileMenu() {
+
+	var mobileNav = document.querySelector('mobile-nav')
+	if (mobileNav.style.display=="block") {
+		mobileNav.style.display=="none";
+	} else {
+		mobileNav.style.display="block";
+	}
+} 
